4918bb814ccf30b4044551f24a4a32fe7da59962
Pin Fung <wpf0709@gmail.com>
2026-06-12 11:29:14 +0800

    update merge sort implementation

---
+++ practical_7/merge_sort.py
@@ -0,0 +1,33 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+
+numbers = [214, 12, 46, 57, 31, 8]
+sorted_numbers = merge_sort(numbers)
+
+print(sorted_numbers)
